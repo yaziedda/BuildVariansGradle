31625fdb23d8d66992b401dc109f01ddf972cbe8
yaziedda <yazieddabr@gmail.com>
2016-06-29 18:03:10 +0700

    Push all

---
+++ app/src/main/jni/key-lb.cpp
@@ -0,0 +1,5 @@
+//
+// Created by root on 6/28/16.
+//
+
+#include "key-lb.h"
---
+++ app/src/main/jni/key-lb.h
@@ -0,0 +1,8 @@
+//
+// Created by root on 6/28/16.
+//
+
+#ifndef BUILDTYPES_KEY_LB_H
+#define BUILDTYPES_KEY_LB_H
+
+#endif //BUILDTYPES_KEY_LB_H
